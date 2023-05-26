e14d25d7bf27d23915ede2d9c231609ae55262eb
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-05-26 01:45:31 -0400

    initial commit

---
+++ queries.js
@@ -0,0 +1,22 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "db.qpbxvieyupoknstmdnur.supabase.co",
+  database: "postgres",
+  port: 6543,
+  password: "hikewell2020!",
+});
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM "Users"', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  getUsers,
+};
